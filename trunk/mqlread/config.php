73ec67e3a8b5f90bd7e0ce36b5b3485a50a6f320
roland.bouman <roland.bouman@9f9dff36-6f20-8428-8ca6-bf3eb45c7d29>
2010-05-13 13:50:30 +0000

    restored config.php to use the sqlite database so everything works *almost* out of the box: people still need to modify the connection-sqlite.json file and set the absolute location of the sakila.sqlite file

--- trunk/mqlread/config.php
+++ trunk/mqlread/config.php
@@ -1,5 +1,15 @@
 <?php
 
-$connection_file_name = '../schema/connection-mysql.json';
-$metadata_file_name = '../schema/schema.json';
-$yui_url = '/yui';
+//The yui_url variable is used only by the query editor.
+//note: by default, the hosted yui resources are used.
+//Set up a local instance of YUI 2 in case you want rely
+//on having access to the internett
+$yui_url = 'http://yui.yahooapis.com/2.8.1';
+
+//note: the file with the connection data contains an absolute path
+//to the sqlite database file. You have to modify that to match the 
+//location on your system or else it won't work!
+$connection_file_name = '../schema/connection-sqlite.json';
+
+$metadata_file_name = '../schema/schema-sqlite.json';
+
